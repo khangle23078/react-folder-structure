27ed2b0eeff69d937fc3501e217499363284c04f
khangle23078 <khangle2307@gmail.com>
2022-12-08 10:59:43 +0700

    create UploadFile component

---
+++ src/components/UploadFile/UploadFile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type UploadFileProps = {}
+
+const UploadFile: React.FC<UploadFileProps> = (props) => {
+   return (
+      <div>UploadFile</div>
+   )
+}
+
+export default UploadFile
